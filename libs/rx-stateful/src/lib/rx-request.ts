f1353c324997ec0eb059a6f0d18012f479571280
Michael Berger <m.gonser@icloud.com>
2025-08-27 07:12:02 +0200

    refactor: rename rx-stateful$.ts to create-state.ts

--- libs/rx-stateful/src/lib/rx-request.ts
+++ libs/rx-stateful/src/lib/rx-request.ts
@@ -1,20 +1,12 @@
-import {
-  Observable,
-  Subject,
-} from 'rxjs';
-import {
-  RxStatefulConfig,
-  RxRequest,
-  RxStatefulSourceTriggerConfig,
-} from './types/types';
-import {_handleSyncValue} from './util/handle-sync-value';
-import {createRxStateful} from './util/create-rx-stateful';
+import { Observable, Subject } from 'rxjs';
+import { RxRequest, RxStatefulConfig } from './types/types';
+import { createRxStateful } from './util/create-rx-stateful';
 import { withRefetchOnTrigger } from './refetch-strategies/refetch-on-trigger.strategy';
-import { createState$ } from './rx-stateful$';
+import { createState$ } from './create-state';
 import { assertInInjectionContext, inject, Injector, runInInjectionContext } from '@angular/core';
 import { RX_STATEFUL_CONFIG } from './config/rx-stateful-config';
 
-export type RxStatefulLoader<T,A,E> = {
+export type RxStatefulLoader<T, A, E> = {
   /**
    * Optional trigger to trigger the requestFn with an argument.
    *
@@ -37,10 +29,7 @@ export type RxStatefulLoader<T,A,E> = {
      */
     operator?: 'switch' | 'merge' | 'concat' | 'exhaust';
   };
-}
-
-
-
+};
 
 /**
  * @publicApi
@@ -59,50 +48,54 @@ export type RxStatefulLoader<T,A,E> = {
  * })
  * @param loaderOptions
  */
-export function rxRequest<T,A, E = unknown>(loaderOptions: RxStatefulLoader<T, A, E>): RxRequest<T, E>{
-  const {requestFn, trigger, config} = loaderOptions;
+export function rxRequest<T, A, E = unknown>(loaderOptions: RxStatefulLoader<T, A, E>): RxRequest<T, E> {
+  const { requestFn, trigger, config } = loaderOptions;
 
   !config?.injector && assertInInjectionContext(rxRequest);
   const assertedInjector = config?.injector ?? inject(Injector);
 
   return runInInjectionContext(assertedInjector, () => {
-    const globalConfig = inject(RX_STATEFUL_CONFIG, {optional: true});
-   // Create internal refresh subject
-   const refreshSubject = new Subject<void>();
+    const globalConfig = inject(RX_STATEFUL_CONFIG, { optional: true });
+    // Create internal refresh subject
+    const refreshSubject = new Subject<void>();
 
-   /**
-    * Merge default config with user provided config
-    */
-   const mergedConfig: RxStatefulConfig<T, E> = {
-       keepValueOnRefresh: false,
-       keepErrorOnRefresh: false,
-       suspenseThresholdMs: 0,
-       suspenseTimeMs: 0,
-       ...globalConfig,
-       ...config,
-       refetchStrategies: [
-         withRefetchOnTrigger(refreshSubject),
-         ...(Array.isArray(config?.refetchStrategies) ? config.refetchStrategies : config?.refetchStrategies ? [config.refetchStrategies] : [])
-       ]
-   };
+    /**
+     * Merge default config with user provided config
+     */
+    const mergedConfig: RxStatefulConfig<T, E> = {
+      keepValueOnRefresh: false,
+      keepErrorOnRefresh: false,
+      suspenseThresholdMs: 0,
+      suspenseTimeMs: 0,
+      ...globalConfig,
+      ...config,
+      refetchStrategies: [
+        withRefetchOnTrigger(refreshSubject),
+        ...(Array.isArray(config?.refetchStrategies)
+          ? config.refetchStrategies
+          : config?.refetchStrategies
+          ? [config.refetchStrategies]
+          : []),
+      ],
+    };
     /**
      * requestFn & !trigger -> source$
      * requestFn & trigger -> sourceFn$ with trigger
      */
-    if (trigger){
+    if (trigger) {
       // @ts-ignore
       mergedConfig.sourceTriggerConfig = {
         trigger,
-        operator: config?.operator ?? 'switch'
-      }
+        operator: config?.operator ?? 'switch',
+      };
     }
 
-   const state$ = createState$<T,A, E>(trigger ? requestFn : requestFn(undefined as A), mergedConfig);
-   const rxStateful = createRxStateful<T, E>(state$, mergedConfig);
+    const state$ = createState$<T, A, E>(trigger ? requestFn : requestFn(undefined as A), mergedConfig);
+    const rxStateful = createRxStateful<T, E>(state$, mergedConfig);
 
-   return {
-       value$: () => rxStateful,
-       refresh: () => refreshSubject.next()
-   };
-  })
+    return {
+      value$: () => rxStateful,
+      refresh: () => refreshSubject.next(),
+    };
+  });
 }
