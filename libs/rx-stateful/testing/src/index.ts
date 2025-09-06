8cdd2207dc951f47ab50b76609abfe8d365cad38
Michael Berger <m.gonser@icloud.com>
2025-09-06 06:08:20 +0200

    refactor(testing): remove deprecated utils
    BREAKING CHANGE

--- libs/rx-stateful/testing/src/index.ts
+++ libs/rx-stateful/testing/src/index.ts
@@ -1,2 +1,1 @@
-export {mockRxStateful, RxStatefulMock} from './lib/mock-rx-stateful$'
-export {mockRxRequest, RxRequestMock} from './lib/mock-rx-request'
+export { mockRxRequest, RxRequestMock } from './lib/mock-rx-request';
