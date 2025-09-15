115d3e47f294b028dfed6ef39fef3f9ffdaadba1
Michael Berger <m.gonser@icloud.com>
2025-09-15 14:51:03 +0200

    refactor(rx-stateful): remove unused function

--- libs/rx-stateful/src/lib/refetch-strategies/refetch-strategy.ts
+++ libs/rx-stateful/src/lib/refetch-strategies/refetch-strategy.ts
@@ -1,26 +1,24 @@
-import {Observable, Subject} from "rxjs";
-
+import { Observable, Subject } from 'rxjs';
 
 export type RefetchStrategyKind = 'trigger__rxStateful' | 'auto__rxStateful';
 
 export type RefetchFn = () => Observable<any> | Subject<any>;
 
 export type RefetchStrategy = {
-    kind: RefetchStrategyKind & string,
-    refetchFn: RefetchFn
-}
+  kind: RefetchStrategyKind & string;
+  refetchFn: RefetchFn;
+};
 
 export type TriggerRefetchStrategy = {
-    kind: 'trigger__rxStateful',
-    refetchFn: RefetchFn
-}
+  kind: 'trigger__rxStateful';
+  refetchFn: RefetchFn;
+};
 
 export type AutoRefetchStrategy = {
-    kind: 'auto__rxStateful',
-    refetchFn: RefetchFn
-}
+  kind: 'auto__rxStateful';
+  refetchFn: RefetchFn;
+};
 
-export function refetchFnFactory(trigger: Observable<any> | Subject<any>){
-    return () => trigger
+export function refetchFnFactory(trigger: Observable<any> | Subject<any>) {
+  return () => trigger;
 }
-export function refetchStrategyFactory(){}
