e61b27f5120764eec83fe3c2770e600715764dbd
Mac Mini <macmini@mac-mini.fritz.box>
2025-09-16 07:39:29 +0200

    fix: remove incorrect and unused type guards
    - Remove isRxStatefulConfigOrObsGuard (unused and too generic)
    - Remove isRxStatefulConfigOrSourceTriggerConfigGuard (incorrect implementation)
    - Remove isObservableOrSubjectGuard (only used by removed guards)
    - Clean up unused imports from rxjs
    
    Closes #50

--- libs/rx-stateful/src/lib/types/guards.ts
+++ libs/rx-stateful/src/lib/types/guards.ts
@@ -1,24 +1,9 @@
-import {RxStatefulConfig, SourceTriggerConfig} from "./types";
-import {isObservable, Observable, Subject} from "rxjs";
+import { SourceTriggerConfig } from './types';
 
-
-export function isObservableOrSubjectGuard(arg: any): arg is Observable<any> | Subject<any>{
-    return isObservable(arg) || arg instanceof Subject;
-}
-
-export function isRxStatefulConfigOrObsGuard<T,E>(arg: any): arg is RxStatefulConfig<T, E>{
-    // write type guard for RxStatefulConfig
-    return !(isObservableOrSubjectGuard(arg))
-}
-
-export function isRxStatefulConfigOrSourceTriggerConfigGuard<T,E>(arg: any): arg is RxStatefulConfig<T, E>{
-    // write type guard for RxStatefulConfig
-    return (arg as any)?.trigger !== undefined
-}
 export function isFunctionGuard(value: any): value is (...args: any[]) => any {
-    return typeof value === 'function';
+  return typeof value === 'function';
 }
 
-export function isSourceTriggerConfigGuard<T>(arg: any): arg is SourceTriggerConfig<T>{
-    return arg?.sourceTriggerConfig !== undefined;
+export function isSourceTriggerConfigGuard<T>(arg: any): arg is SourceTriggerConfig<T> {
+  return arg?.sourceTriggerConfig !== undefined;
 }
