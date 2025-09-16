6bd6644f4b6e858e9eee3030ec3d5a623c3392fc
Michael Berger <m.gonser@icloud.com>
2025-09-16 15:17:36 +0200

    refactor(rx-stateful): preserve todo comment

--- libs/rx-stateful/src/lib/util/calc-start-value-for-refresh.ts
+++ libs/rx-stateful/src/lib/util/calc-start-value-for-refresh.ts
@@ -1,11 +1,12 @@
-import {InternalRxState, RxStatefulConfig} from "../types/types";
+import { InternalRxState, RxStatefulConfig } from '../types/types';
 
-export function calcStartValueForRefresh<T, E>(mergedConfig: RxStatefulConfig<T,E>): Partial<InternalRxState<T, E>>{
-  let baseStartWithValue: Partial<InternalRxState<T, E>> = { isLoading: true, isRefreshing: true, context: 'suspense'};
+export function calcStartValueForRefresh<T, E>(mergedConfig: RxStatefulConfig<T, E>): Partial<InternalRxState<T, E>> {
+  // TODO for first emission set isRefreshing to false
+  let baseStartWithValue: Partial<InternalRxState<T, E>> = { isLoading: true, isRefreshing: true, context: 'suspense' };
   if (!mergedConfig?.keepValueOnRefresh) {
     baseStartWithValue = { ...baseStartWithValue, value: null };
   }
-  if(!mergedConfig?.keepErrorOnRefresh) {
+  if (!mergedConfig?.keepErrorOnRefresh) {
     baseStartWithValue = { ...baseStartWithValue, error: undefined };
   }
 
