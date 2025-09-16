e194f3e09b96cf602094020d99ea430cfb9eabbc
Mac Mini <macmini@mac-mini.fritz.box>
2025-09-16 07:13:53 +0200

    refactor: normalize injection tokens to standard Angular pattern
    - Replace function-based tokens with standard InjectionToken instances
    - Update RX_STATEFUL_CONFIG to use direct token instead of function
    - Update RX_STATEFUL_CLIENT_CONFIG to use direct token instead of function
    - Update provider functions to use new tokens directly

--- libs/rx-stateful/experimental/src/lib/client/config/with-config.ts
+++ libs/rx-stateful/experimental/src/lib/client/config/with-config.ts
@@ -1,8 +1,8 @@
-import {inject, InjectionToken} from '@angular/core';
+import { inject, InjectionToken } from '@angular/core';
 import { makeFeature } from './config-feature';
-import { Config, RX_STATEFUL_CONFIG } from '@angular-kit/rx-stateful';
+import { Config } from '@angular-kit/rx-stateful';
 
-export const RX_STATEFUL_CLIENT_CONFIG = <T,E>() => new InjectionToken<Config<T, E>>('RX_STATEFUL_CONFIG');
+export const RX_STATEFUL_CLIENT_CONFIG = new InjectionToken<Config<any, any>>('RX_STATEFUL_CLIENT_CONFIG');
 
 export function withConfig<T, E>(config: Config<T, E>) {
   return makeFeature('Config', [{ provide: RX_STATEFUL_CLIENT_CONFIG, useValue: config }]);
--- libs/rx-stateful/src/lib/config/rx-stateful-config.ts
+++ libs/rx-stateful/src/lib/config/rx-stateful-config.ts
@@ -1,17 +1,21 @@
-import {InjectionToken, makeEnvironmentProviders} from '@angular/core';
+import { InjectionToken, makeEnvironmentProviders } from '@angular/core';
 import { RxStatefulConfig } from '../types/types';
 import { RefetchStrategy } from '../refetch-strategies/refetch-strategy';
 
 export type Config<T, E> = Pick<
   RxStatefulConfig<T, E>,
-  'keepErrorOnRefresh' | 'keepValueOnRefresh' | 'errorMappingFn' | 'beforeHandleErrorFn' | 'accumulationFn' | 'suspenseTimeMs' | 'suspenseThresholdMs'
+  | 'keepErrorOnRefresh'
+  | 'keepValueOnRefresh'
+  | 'errorMappingFn'
+  | 'beforeHandleErrorFn'
+  | 'accumulationFn'
+  | 'suspenseTimeMs'
+  | 'suspenseThresholdMs'
 > & {
   autoRefetch?: RefetchStrategy;
 };
-export const RX_STATEFUL_CONFIG = <T,E>() => new InjectionToken<Config<T, E>>('RX_STATEFUL_CONFIG');
+export const RX_STATEFUL_CONFIG = new InjectionToken<Config<any, any>>('RX_STATEFUL_CONFIG');
 
 export function provideRxStatefulConfig<T, E>(config: Partial<Config<T, E>>) {
-  return makeEnvironmentProviders([{ provide: RX_STATEFUL_CONFIG<T, E>, useValue: config }]);
+  return makeEnvironmentProviders([{ provide: RX_STATEFUL_CONFIG, useValue: config }]);
 }
-
-
